c1e9bdea104aa209d06ad7153e2aa971e69c8e94
nimigeanconstantinion <constantin.nimigean@gmail.com>
2025-11-05 12:34:20 +0200

    commit cu modif la vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,36 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist/ui',
-    emptyOutDir: true
+    emptyOutDir: true,
   },
   resolve: {
     alias: {
-      '@': path.resolve( __dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  base: '/ui/', // ✅ frontendul e servit sub /ui prin gateway
   server: {
-    port: 3000,     // 👈 Portul dorit
-    open: true,
-    host:true// opțional, deschide browserul automat
+    host: '0.0.0.0', // ✅ merge în container și local
+    port: 3000,
+    allowedHosts: [
+      'localhost', // pentru rulare locală
+      'client',    // pentru rulare în container (numele serviciului)
+    ],
+    cors: true, // 🔓 opțional, dar util dacă faci API calls
   },
-});
+}) ;
+//   build: {
+//     outDir: 'dist/ui',
+//     emptyOutDir: true
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve( __dirname, './src'),
+//     },
+//   },
+//   server: {
+//     port: 3000,     // 👈 Portul dorit
+//     open: true,
+//     host:true// opțional, deschide browserul automat
+//   },
+// });
